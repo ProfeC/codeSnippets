81679653d0e463c267435d8a5c7c32bb5fc7e5a4
G. Lee Clark, II <gclark2@gmail.com>
2013-06-10 12:31:44 -0400

    Update CommonSpot-SitePage-CheckDupicates.sql

--- SQL/CommonSpot-SitePage-CheckDupicates.sql
+++ SQL/CommonSpot-SitePage-CheckDupicates.sql
@@ -3,11 +3,14 @@
 -- regardless of case. It is useful when migrating sites from a db structure that is case insensitive to one that is.
 
 select *
+
 from sitepages
+
 where lower(name) in (
    select lower(name)
    from sitepages
    group by lower(name), audienceid, subsiteid, pagetype
    having count(name) > 1
 )
+
 order by lower(name), subsiteid
