316a5da1ac4e9e62dba22a84bb8da6bca00c971d
G. Lee Clark, II <gclark2@gmail.com>
2013-06-10 14:04:48 -0300

    Update CommonSpot-SitePage-CheckDupicates.sql
    select distinct creators along with a select few columns.

--- SQL/CommonSpot-SitePage-CheckDupicates.sql
+++ SQL/CommonSpot-SitePage-CheckDupicates.sql
@@ -2,7 +2,7 @@
 -- The results of this query will show entries in the sitepages table where the name (file name) is similar, 
 -- regardless of case. It is useful when migrating sites from a db structure that is case insensitive to one that is.
 
-select *
+select distinct creatorid, id, name, subsiteid, ownerid, description, caption
 
 from sitepages
 
@@ -12,5 +12,3 @@ where lower(name) in (
    group by lower(name), audienceid, subsiteid, pagetype
    having count(name) > 1
 )
-
-order by lower(name), subsiteid
